2d3b4705632ce18c7fa46834add393085f36c225
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-03-14 12:11:21 +0000

    add loadTable function

--- src/app/pages/admin-pages/point-of-drop-management/point-of-drop-management.component.ts
+++ src/app/pages/admin-pages/point-of-drop-management/point-of-drop-management.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { RegisterPointOfDropComponent } from 'src/app/components/register-point-of-drop/register-point-of-drop.component';
+import { AdminConsumerService } from 'src/app/services/api-consumer/api-admin-consumer.service';
 
 @Component({
   selector: 'app-point-of-drop-management',
   templateUrl: './point-of-drop-management.component.html',
   styleUrls: ['./point-of-drop-management.component.scss']
 })
-export class PointOfDropManagementComponent implements OnInit{
-  constructor(private modalService: NzModalService) {}
+export class PointOfDropManagementComponent implements OnInit {
+  data: any;
+  constructor(private modalService: NzModalService, private adminConsumer: AdminConsumerService) { }
 
   ngOnInit(): void {
-      
+    this.loadTable();
   }
 
   openModal() {
@@ -21,5 +23,19 @@ export class PointOfDropManagementComponent implements OnInit{
       nzFooter: null
     };
     const modalRef = this.modalService.create(modalConfig);
+  }
+
+  loadTable() {
+    this.adminConsumer.getAllPointOfDrop().subscribe({
+      next: (response: any) => {
+        console.log(response);
+        if (response.status == 200) {
+          this.data = response.data;
+        }else {}
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    })
   }
 }
