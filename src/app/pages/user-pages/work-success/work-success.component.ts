9f52fdbdf9845b2d2d6e9ee177a635a0dc6bc7fa
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-03-19 18:34:43 +0000

    implement work success

--- src/app/pages/user-pages/work-success/work-success.component.ts
+++ src/app/pages/user-pages/work-success/work-success.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserConsumerService } from 'src/app/services/api-consumer/api-user-consumer.service';
 
 @Component({
   selector: 'app-work-success',
   templateUrl: './work-success.component.html',
   styleUrls: ['./work-success.component.scss']
 })
-export class WorkSuccessComponent {
+export class WorkSuccessComponent implements OnInit {
+  data: any
+  constructor(private userConsumer: UserConsumerService) { }
 
+  ngOnInit(): void {
+    this.userConsumer.getListOfUserAssignments('completed').subscribe({
+      next: (response: any)=>{
+        console.log(response);
+        if(response.status == 200) {
+          this.data = response.data;
+        }
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    })
+  }
 }
